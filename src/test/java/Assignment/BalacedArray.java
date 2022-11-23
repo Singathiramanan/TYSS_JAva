51ef7ff0be6704f287d53d2708945eb0f059c754
Umamani <Umamani@DESKTOP-EM3OVKO>
2022-11-23 10:49:43 +0530

    new program balacedArray

---
+++ src/test/java/Assignment/BalacedArray.java
@@ -0,0 +1,34 @@
+package Assignment;
+
+public class BalacedArray {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr={5,6,8,11};
+		int lcount=0,rcount=0;
+	    if(arr.length>2){
+	        for(int i=0;i<arr.length-1;i++){
+	  
+	            lcount=lcount+arr[i];
+	            System.out.println(lcount);
+	            if(lcount==arr[i+2]){
+	                for(int j=i+2;j<arr.length;j++){
+	         
+	                    rcount=rcount+arr[j];
+	                    
+	                }
+	                break;
+	            }
+	        }
+	        if(lcount==rcount){
+	            System.out.println("yes");
+	        }else{
+	            System.out.println("no");
+	        }
+	    }else{
+	        System.out.println("no");
+	    }
+	}
+
+}
