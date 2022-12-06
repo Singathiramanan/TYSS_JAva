70786f7d9d5804aed5ba2f1e646a1f18447af67d
Singathiramanan <singathiramanan@yahoo.com>
2022-12-06 18:05:08 +0530

    update

---
+++ src/test/java/Iterview_pgm/sdfghjk.java
@@ -0,0 +1,47 @@
+package Iterview_pgm;
+
+public class sdfghjk {
+
+	public static void main(String[] args) {
+		// int []a={4,7,9,8,76,43,23,56,37};
+		// for(int i=0;i<a.length;i++){
+		// int count=0;
+		// for(int j=2;j<a[i];j++){
+		// if(a[i]%j==0){
+		// count++;}
+		// }
+		// if(count==0){
+		// System.out.println(a[i]+" prime");
+		// }else{
+		// System.out.println(a[i]+" not prime");
+		// }
+		// }
+		/*int[] b = { 121, 345, 434, 589, 999, 675, 567 };
+		for (int i = 0; i < b.length; i++) {
+			int no = b[i];
+			int copy = 0;
+			while (no != 0) {
+				int rem = no % 10;
+				copy = rem + (copy * 10);
+				no = no / 10;
+			}
+			if (copy == b[i]) {
+				System.out.println(b[i] + " pali");
+			} else {
+				System.out.println(b[i] + " not pali");
+			}
+		}*/
+		int no=1473,copy=no,sum=0;
+		while(no!=0){
+			int rem=no%10;
+			sum=sum+(rem*rem*rem);
+			no=no/10;
+		}
+		if(sum==copy){
+			System.out.println("armstrong");
+		}else{
+			System.out.println("not armstrong");
+		}
+	}
+
+}
