cd233685bbaf2cea0f86b189b140331d889d6b54
Singathiramanan <singathiramanan@yahoo.com>
2023-01-30 17:42:13 +0530

    update

--- src/test/java/Practice/interfaceExecution.java
+++ src/test/java/Practice/interfaceExecution.java
@@ -2,9 +2,9 @@ package Practice;
 
 public interface interfaceExecution {
 	String dfgh="";
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-		System.out.println(dfgh);
-	}
+	// public static void main(String[] args) {
+	// 	System.out.println("Hello world!");
+	// 	System.out.println(dfgh);
+	// }
 	public void fghj();
 }
