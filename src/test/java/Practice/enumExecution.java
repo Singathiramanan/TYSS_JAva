2e6d132d2256b8fedc5bb4146c8b91d72500ef06
Singathiramanan <singathiramanan@yahoo.com>
2023-01-31 18:05:43 +0530

    update

--- src/test/java/Practice/enumExecution.java
+++ src/test/java/Practice/enumExecution.java
@@ -1,10 +1,13 @@
 package Practice;
 
 public enum enumExecution {
-		
-	no;
+		gh;
+	String no="one";
 	public static void main(String[] args) {
+		String no="two";
+		System.out.println(gh.no);
 		System.out.println(no);
+		System.out.println(enumExecution.gh.no);
 	}
 	
 }
--- src/test/java/Practice/interfaceExecution.java
+++ src/test/java/Practice/interfaceExecution.java
@@ -1,10 +1,14 @@
 package Practice;
 
 public interface interfaceExecution {
-	String dfgh="";
-	// public static void main(String[] args) {
-	// 	System.out.println("Hello world!");
-	// 	System.out.println(dfgh);
-	// }
-	public void fghj();
+	String dfgh="one";
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println(dfgh);
+		
+	}
+	public default void fghj(){
+		System.out.println("two");
+	}
 }
